d57bbb22086441ae6414e03ac0d8bb9d10a8b188
lgroenhart <luuc_groenhart@hotmail.com>
2026-04-08 13:48:18 +0200

    documentation of funcs.py and rename to data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,4 @@
-# functions py file for AMDFLOW
+# data_utils py file for AMDFLOW
 # contains all helper functions to clip, transform and treat data before model run
 from openmindat import LocalitiesRetriever, GeomaterialRetriever
 import os
@@ -15,7 +15,23 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
 def mindat_collector(region, material_id = 3314, mineral_strings = "(Fe|S)", material_name = "pyrite", path_str = "../data/", mindat_api_str = "mindat_API_key.txt"):
+    """Mindat data collector, queries mindat API in specific region for a specific mineral and checks if those minerals are located at a mine/quarry, saves locations at lat/lon csv file
 
+    Parameters
+    ----------
+    region : str
+        string matching the mindat region to query
+    material_id : int, optional
+        mindat material id corresponding to a specific material, by default 3314 (pyrite id)
+    mineral_strings : str, optional
+        mindat string corresponding to specific chemicals using regex expression, specify full compound with string1|string2, always place full string in brackets (), by default "(Fe|S)"
+    material_name : str, optional
+        mindat name of material, by default "pyrite"
+    path_str : str, optional
+        string of where function should place "mindat_data/" folder wherein output csv files are places and checked, by default "../data/"
+    mindat_api_str : str, optional
+        string of where mindat API key file with API key is located, by default "mindat_API_key.txt"
+    """
     # set API key
     with open(mindat_api_str) as f:
         key = f.read()
@@ -70,6 +86,17 @@ def mindat_collector(region, material_id = 3314, mineral_strings = "(Fe|S)", mat
     return
 
 def vector_rasterisation(output_path = "../data/mines_raster.tif", flo1k_path = "../data/flo_IPB_2015.nc", vector_path = "../data/mine_polygons/74548_projected polygons.shp"):
+    """Rasterises vector file to a reference raster and saves rasterised file
+
+    Parameters
+    ----------
+    output_path : str, optional
+        string of where output raster file should be output to, by default "../data/mines_raster.tif"
+    flo1k_path : str, optional
+        string of where reference raster (reference for the vector to raster transform) is located, by default "../data/flo_IPB_2015.nc"
+    vector_path : str, optional
+        string of where vector, to be rasterised, is located, by default "../data/mine_polygons/74548_projected polygons.shp"
+    """
     # rasterise mining polygons
     # code sourced from: K. Haven 2019 https://medium.com/data-science/use-python-to-convert-polygons-to-raster-with-gdal-rasterizelayer-b0de1ec3267
     nc_path = flo1k_path
@@ -119,6 +146,22 @@ def flo1k_prep(flo1k_path = "../data/FLO1K.ts.1960.2015.qav.nc",
                basins_iloc = (45, 53),
                date = np.datetime64("2015-01-01"),
                output_path = f"../data/flo_IPB_"):
+    """Function to clip the full flo1k dataset to a specified area of interest and dates, saves clipped flo1k to new netcdf file
+
+    Parameters
+    ----------
+    flo1k_path : str, optional
+        string of where flo1k dataset is located, by default "../data/FLO1K.ts.1960.2015.qav.nc"
+    basins_path : str, optional
+        string of where HydroBASINC data is located, used for clipping of flo1k dataset to area of interest, by default "../data/hybas_eu_lev01-06_v1c/hybas_eu_lev04_v1c.shp"
+    basins_iloc : tuple, optional
+        tuple of which ilocs of the hydrobasins dataset should be used for clipping the flo1k dataset, by default (45, 53)
+    date : np.datetime64 OR np.ndarray, optional
+        np.datetime64 type object of what dates (1960-2015) the clipped flo1k dataset should save, if more than one should be used use np.ndarray, by default np.datetime64("2015-01-01") OR
+        np.ndarray of np.datetime64 objects of what dates (1060-2015) the clipped flo1k dataset should save
+    output_path : str, optional
+        string of where the clipped flo1k dataset should be saved, date is used to append to this string to get full output path, by default f"../data/flo_IPB_"
+    """
     
     
     flo_one_k = xr.open_dataset(flo1k_path)
@@ -146,6 +189,20 @@ def flo1k_prep(flo1k_path = "../data/FLO1K.ts.1960.2015.qav.nc",
     flo_aoi_date.to_netcdf(output_path)
 
 def hydir_IDs(ds, aoi):
+    """Function to convert HydroSHEDS direction dataset to dataset containing unique cell IDs per cell and the ID of where the outflow from that cell goes to
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset of hydroSHEDS direction dataset to convert to unique cell IDs, and outflow IDs dataset
+    aoi : geopandas.GeoDataFrame
+        GeoDataFrame of area of interest that the input dataset should be clipped to
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        Dataset of unique IDs per cell (ID) and where the outflow of each cell goes to (which ID the outflow from cells goes to) (outID)
+    """
     # extract band of 2d shape
     if "band" in ds["hydir"].dims:
         hydir_2d = ds["hydir"].isel(band=0, drop=True)
@@ -246,6 +303,23 @@ def hydir_IDs(ds, aoi):
     return ds
 
 def filter_mines_with_buffer(raster, points, buffer_deg = 0.045):
+    """Function to filter a full raster dataset with mindat mineral location, mindat mineral location gets buffer: 
+        if mine cell of raster falls within buffer the cell passes filter, otherwise cells gets dropped
+
+    Parameters
+    ----------
+    raster : xarray.Dataset
+        Dataset of mines cells
+    points : geopandas.GeoDataFrame
+        GeoDataFrame of mindat mineral locations to link with mines
+    buffer_deg : float, optional
+        !high uncertainty!, float of degree buffer in WG84 to buffer the points, by default 0.045
+
+    Returns
+    -------
+    raster_filtered : xarray.Dataset
+        Dataset of mines that fall within buffer of points, thus likely containing the mineral of interest
+    """
     points.set_geometry(col = 0)
 
     # create buffer in degrees 
@@ -258,13 +332,40 @@ def filter_mines_with_buffer(raster, points, buffer_deg = 0.045):
     return raster_filtered
 
 def bool_to_int(ds, dtype="int16"):
-    """Convert all boolean data variables to the specified integer type."""
+    """Convert all boolean data variables to the specified integer type (not tested with other dtypes than int16), 
+        used to save memory as size bool dataset > size int16 dataset
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset of bools
+    dtype : str, optional
+        string of dtype to convert to, by default "int16"
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        Dataset of dtype
+    """
+
     for var in ds.data_vars:
         if ds[var].dtype == bool:
             ds[var] = ds[var].astype(dtype)
     return ds
 
 def cleanup_and_metadata(ds):
+    """Function to cleanup dataset and add metadata
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset to cleanup and add metadata to
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        Cleaned dataset with metadata
+    """
     ds = ds.drop_vars(["band", "hydir"])
     ds["mines"].attrs = {"units": "none", "description": "mapping of mines, all non nan are mines"}
     ds = ds.assign_coords(x=("x", ds.lon.values), y=("y", ds.lat.values))
@@ -274,6 +375,23 @@ def cleanup_and_metadata(ds):
     return ds
 
 def add_time(ds, length, frequency):
+    """Function to add more discrete timestep to dataset with flo1k annual streamflow (Q) data, Q gets divided by by length to represent mean annual streamflow divided by discrete timestep,
+        returns new dataset
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset to add more discrete timestep to
+    length : int
+        int of length of how many of the discrete timesteps in a year there are: e.g; 52 (weekly timesteps), 12 (monthly timesteps), (365) (daily timesteps)
+    frequency : str
+        str of what length type (weekly, monthly, daily, etc.) the discrete timestep is, see pandas.data_range() for options
+
+    Returns
+    -------
+    ds_new : xarray.Dataset
+        Dataset with years divided into more discrete timesteps where Q (streamflow) is divided by length to represent mean annual streamflow divided by discrete timestep
+    """
     start = ds.time.values[0]
     n_years = len(ds.time)
     date_range = pd.date_range(start, periods=n_years * length, freq=frequency)
@@ -295,11 +413,11 @@ def add_time(ds, length, frequency):
     )
     Q_new = Q_new / length
 
-    # Start with Q, then add back every other data variable from the original
+    # start with Q, then add back every other data variable from the original
     ds_new = xr.Dataset({"Q": Q_new})
     for var in ds.data_vars:
         if var != "qav":
-            # Repeat static variables to match the new time dimension
+            # repeat static variables to match the new time dimension
             if "time" in ds[var].dims:
                 var_repeated = np.repeat(ds[var].values, length, axis=0)
                 ds_new[var] = xr.DataArray(
@@ -310,25 +428,51 @@ def add_time(ds, length, frequency):
             else:
                 ds_new[var] = ds[var]
 
-    # Restore non-time scalar coordinates (e.g. spatial_ref)
+    # restore non-time scalar coordinates (
     for coord in ds.coords:
         if coord not in ("time", "lat", "lon") and coord not in ds_new.coords:
             ds_new[coord] = ds[coord]
 
     return ds_new
 
-def estimate_ore(ds, F):
-    # uses ds["mines"] values, might be useful when it is filled with area, but not right now
-    #ds["ore"] = ds["mines"].where(ds["mines"].notnull(), 0) * (H * ((4 * 1000) / 4) * F * 27)
+def estimate_ore(ds, F, ox_range = 27):
+    """Function convert a dataset with only if mines are there (dataset["mines"], 1 = mine, 0 = no mine) to a dataset containing variable "ore" which has an estimate ore amount in square metre 
 
-    constant = 27000 * F
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset containing "mines" variable of int type with 1 = mine, 0 = no mine, dataset should be 1km*1km / 3 arc second resolution, otherwise the constant equation should be changed
+    F : float
+        fraction of reactive material per square metre of mine, used in the calculation: ox_range * 1000 * F
+    ox_range : int, optional
+        range of square metre of mineral exposed to real sqaure metre of mineral that can be oxidised, represents fractures and oxidation range, by default 27, but ranges between 27-161 are reported
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        Dataset containing "ore" variable with estimated ore amount in square metre
+    """
+
+    constant = ox_range * 1000 * F
     ds["ore"] = xr.where(ds["mines"].notnull(), constant, np.nan)
     ds["ore"].attrs = {"description": "Estimation of reactive ore per cell", "unit": "m2",}
     ds = ds.drop_vars(["mines"])
     return ds
 
 def animation_plot(dataarray, aoi = None, size = (10, 8), cmap = "Reds"):
+    """Function to animate timeseries dataset of AMDFLOW
 
+    Parameters
+    ----------
+    dataarray : xarray.Dataarray
+        Dataarray of AMDFLOW output of one variable 
+    aoi : geopandas.GeoDataFrame, optional
+        area of interest to plot the boundaries of, usually a region/watershed, by default None
+    size : tuple, optional
+        size of plot in inches, by default (10, 8)
+    cmap : str, optional
+        colour map of plot, see matplotlib cmap documentatio, by default "Reds"
+    """
     def _ani_update(t):
         im.set_data(plot_data[t].values)
         ax.set_title(f"{date_range[t][0]}/{date_range[t][1]}/{date_range[t][2]}")
@@ -364,41 +508,44 @@ def vector_to_raster_sens(raster_file, vector_file):
 
     return
 
-def sel_nearest_where(da, lat, lon, condition):
+def sel_nearest_where(da, lat, lon, condition, max_distance_km=5, dist_km=None):
     """
-    Select the nearest lat/lon point in ds[var] that satisfies condition.
-    
-    Parameters
-    ----------
-    ds        : xr.Dataset
-    var       : variable name to apply condition to
-    lat, lon  : target coordinates
-    condition : callable applied to ds[var], e.g. lambda x: x > 0
+    Select nearest grid cell where condition is met, within max distance.
+    Optionally accepts a precomputed dist_km array to avoid redundant computation.
+    Returns both the data and the distance.
     """
     lat = float(lat)
     lon = float(lon)
-    # mask to valid cells only
-    valid = condition(da)           # DataArray of bools
-    da_valid = da.where(valid)           # NaN everywhere condition is False
 
-    lats = da.lat.values.astype(float)
-    lons = da.lon.values.astype(float)
+    # Create boolean mask for valid cells
+    valid = condition(da)
+    valid_mask = valid.any(dim="time").values if "time" in valid.dims else valid.values 
 
-    # compute distance from target point to every grid cell
-    dlat = lats - lat
-    dlon = lons - lon
-    dlat_2d, dlon_2d = np.meshgrid(dlat, dlon, indexing = "ij")
-    dist = np.sqrt(dlat_2d**2 + dlon_2d**2)   # shape (lat, lon)
+    if dist_km is None: 
+        lats = da.lat.values.astype(float)
+        lons = da.lon.values.astype(float)
+        dlat_2d, dlon_2d = np.meshgrid(lats - lat, lons - lon, indexing="ij")
+        dlat_km = dlat_2d * 111
+        dlon_km = dlon_2d * 111 * np.cos(np.radians(lat))
+        dist_km = np.sqrt(dlat_km**2 + dlon_km**2)
 
-    # mask distances where condition is not met
-    valid_mask = valid.any(dim="time") if "time" in valid.dims else valid
-    dist[~valid_mask] = np.nan
+    dist_masked = dist_km.astype(float).copy()  
+    dist_masked[~valid_mask | (dist_km > max_distance_km)] = np.nan  
 
-    # find the lat/lon of the minimum valid distance
-    flat_idx  = np.nanargmin(dist)
-    best_lat_idx, best_lon_idx = np.unravel_index(flat_idx, dist.shape)
+    if np.all(np.isnan(dist_masked)):
+        dist_fallback = dist_km.astype(float).copy()
+        dist_fallback[dist_km > max_distance_km] = np.nan
+        if np.all(np.isnan(dist_fallback)):
+            print(f"Warning: No cells within {max_distance_km} km — using absolute nearest.")
+            flat_idx = np.nanargmin(dist_km)
+        else:
+            flat_idx = np.nanargmin(dist_fallback)
+    else:
+        flat_idx = np.nanargmin(dist_masked)
 
-    best_lat = float(lats[best_lat_idx])
-    best_lon = float(lons[best_lon_idx])
+    best_lat_idx, best_lon_idx = np.unravel_index(flat_idx, dist_km.shape)
+    best_lat = float(da.lat.values[best_lat_idx])
+    best_lon = float(da.lon.values[best_lon_idx])
+    actual_distance = dist_km[best_lat_idx, best_lon_idx]
 
-    return da_valid.sel(lat=best_lat, lon=best_lon)
+    return da.sel(lat=best_lat, lon=best_lon), actual_distance
